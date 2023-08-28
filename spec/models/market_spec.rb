a7109a4ee6d5ea1ae755d895bfe9e9ab85c0be44
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2023-08-28 13:52:53 -0600

    initial commit

---
+++ spec/models/market_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Market, type: :model do
+  it { should have_many(:market_vendors) }
+  it { should have_many(:vendors).through(:market_vendors) }
+
+  describe '#vendor_count' do
+    it 'returns the number of vendors associated with the market' do
+
+      expect(market.vendor_count).to eq(3)
+    end
+  end
+end
