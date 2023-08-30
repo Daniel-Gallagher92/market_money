86207ce7ec1ef72e748d5a68d86299691c358dad
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2023-08-30 16:05:14 -0600

    Feat 8 happy/sad complete, still need to figure out 422 error

--- spec/requests/api/v0/market_vendors_request_spec.rb
+++ spec/requests/api/v0/market_vendors_request_spec.rb
@@ -58,4 +58,103 @@ RSpec.describe "Vendor API", type: :request do
       expect(nope[:errors].first[:details]).to eq("Couldn't find Market with 'id'=123123123123")
     end
   end
+
+  describe "POST /api/v0/market_vendors" do
+    it "can create a new market_vendor with both valid id's" do 
+      market_1 = create(:market)
+      vendor_1 = create(:vendor)
+
+      market_vendor_params = ({
+        market_id: market_1.id,
+        vendor_id: vendor_1.id
+      })
+
+      headers = { "CONTENT_TYPE" => "application/json" }
+      
+      expect(MarketVendor.exists?).to eq(false)
+
+      post "/api/v0/market_vendors", headers: headers, params: JSON.generate(market_vendor_params)
+
+      expect(response).to have_http_status(201)
+      expect(MarketVendor.exists?).to eq(true)
+
+      # get "/api/v0/markets/#{market_1.id}/vendors" #when I uncomment this line, the test fails. Why?
+      
+      # expect(response.status).to eq(200)
+
+      new_market_vendor = JSON.parse(response.body, symbolize_names: true)[:data]
+
+      expect(new_market_vendor[:attributes]).to have_key(:market_id)
+      expect(new_market_vendor[:attributes][:market_id]).to eq(market_1.id)
+      
+      expect(new_market_vendor[:attributes]).to have_key(:vendor_id)
+      expect(new_market_vendor[:attributes][:vendor_id]).to eq(vendor_1.id)
+    end
+
+    it 'returns a 404 if invalid market id is passed in' do 
+      vendor_1 = create(:vendor)
+
+      market_vendor_params = ({
+        market_id: 123456543,
+        vendor_id: vendor_1.id
+      }) 
+
+      headers = { "CONTENT_TYPE" => "application/json" }
+
+      post "/api/v0/market_vendors", headers: headers, params: JSON.generate(market_vendor_params)
+
+      expect(response).to have_http_status(404)
+      
+      nope = JSON.parse(response.body, symbolize_names: true)[:errors].first[:details]
+
+      expect(nope).to eq("Validation failed: Market must exist")
+    end
+
+    it 'returns a 404 if invalid vendor id is passed in' do 
+      market = create(:market)
+
+      market_vendor_params = ({
+        market_id: market.id,
+        vendor_id: 123456543
+      })
+
+      headers = { "CONTENT_TYPE" => "application/json" }
+
+      post "/api/v0/market_vendors", headers: headers, params: JSON.generate(market_vendor_params)
+
+      expect(response).to have_http_status(404)
+
+      nope = JSON.parse(response.body, symbolize_names: true)[:errors].first[:details]
+
+      expect(nope).to eq("Validation failed: Vendor must exist")
+    end
+
+    it 'returns a 400 if no market/vendor params are passed in' do
+      params = { }
+
+      headers = { "CONTENT_TYPE" => "application/json" }
+
+      post "/api/v0/market_vendors", headers: headers, params: JSON.generate(market_vendor: params)
+
+      expect(response).to have_http_status(400)
+
+      nope = JSON.parse(response.body, symbolize_names: true)[:errors].first[:details]
+
+      expect(nope).to eq("param is missing or the value is empty: market_vendor")
+    end
+
+    # it "returns 422 when trying to create a market_vendor that already exists" do 
+    #   market = create(:market)
+    #   vendor = create(:vendor)
+    #   market_vendor = MarketVendor.create!(market_id: market.id, vendor_id: vendor.id)
+
+    #   params = { market_id: market.id, vendor_id: vendor.id }
+    #   headers = { "CONTENT_TYPE" => "application/json" }
+    #   post "/api/v0/market_vendors", headers: headers, params: JSON.generate(market_vendor: params)
+
+    #   expect(response).to have_http_status(422)
+    
+    # end
+
+  end
 end
