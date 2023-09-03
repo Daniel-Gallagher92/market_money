32f8145b484fe31dbf548a754dfa290f9b8513f2
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2023-09-03 10:01:11 -0600

    add params to nearest_atm_service_method

--- app/services/nearest_atm_service.rb
+++ app/services/nearest_atm_service.rb
@@ -2,15 +2,17 @@ class NearestAtmService
 
   def self.nearest_atms(market) 
     response = conn.get do |req|
-      req.url "search/2/nearbySearch/.json",lat: market.lat, lon: market.lon
+      req.url "search/2/nearbySearch/.json"
+      req.params[:lat] = market.lat
+      req.params[:lon] = market.lon
+      req.params[:radius] = 1000
       req.params['categorySet'] = 7397
     end
     JSON.parse(response.body, symbolize_names: true)
-
   end
 
   def self.get_url(url) 
-    response = conn.get(url)
+    @@url ||= response = conn.get(url) #Am I using this right?
     JSON.parse(response.body, symbolize_names: true)
   end
 
